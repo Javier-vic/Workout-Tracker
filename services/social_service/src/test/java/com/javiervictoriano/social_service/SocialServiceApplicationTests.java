44e571072fc56842cf771e93196e56ea7f949883
Javier Victoriano <93997406+Javier-vic@users.noreply.github.com>
2025-02-12 16:03:15 -0300

    feat(backend): create  the social_service spring boot project
    refers to #3

---
+++ services/social_service/src/test/java/com/javiervictoriano/social_service/SocialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javiervictoriano.social_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
